39f384628231118caacc879feeed761b8f3482bd
Tong Yu <rachieyu@gmail.com>
2018-05-29 11:50:16 +1000

    created game logic-matching

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards =['queen','queen','king','king'];
+var cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
 
+if(cardsInPlay.length ===2) {
+  if (cardsInPlay[0]===cardsInPlay[1]) {
+     alert("You found a match!");
+  } else
+     alert("Sorry, try again");
+}
