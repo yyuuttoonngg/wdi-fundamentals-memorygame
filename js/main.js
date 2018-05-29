5cc8b14b2cc6a901e09d9d99674c224b444f94e9
Tong Yu <rachieyu@gmail.com>
2018-05-29 13:40:08 +1000

    add flipcard function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 var cards =['queen','queen','king','king'];
 var cardsInPlay = [];
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
+var checkForMatch = function(){
+	if(cardsInPlay.length ===2) {
+	  if (cardsInPlay[0]===cardsInPlay[1]) {
+	     alert("You found a match!");
+	  } else
+	     alert("Sorry, try again");
+	}
+};
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+    checkForMatch();	
+};
+flipCard(0);
+flipCard(2);
 
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
 
-if(cardsInPlay.length ===2) {
-  if (cardsInPlay[0]===cardsInPlay[1]) {
-     alert("You found a match!");
-  } else
-     alert("Sorry, try again");
-}
