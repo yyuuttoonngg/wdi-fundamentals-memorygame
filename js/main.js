780f08c82aa9ae87dd1bfd61c8c5cd2268327c9c
Tong Yu <rachieyu@gmail.com>
2018-05-30 17:02:59 +1000

    add card suit and image objects to array

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards =['queen','queen','king','king'];
+var cards =[
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage:  "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if(cardsInPlay.length ===2) {
@@ -9,11 +30,12 @@ var checkForMatch = function(){
 	}
 };
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
     checkForMatch();	
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 };
+
 flipCard(0);
-flipCard(2);
-
-
+flipCard(1);
