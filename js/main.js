1b23ca9a29d69285b18836928c20d0a52e05589e
Tong Yu <rachieyu@gmail.com>
2018-05-31 15:20:23 +1000

    created game-board by DOM and add flipped card images

--- js/main.js
+++ js/main.js
@@ -20,7 +20,24 @@ suit: "diamonds",
 cardImage:"images/king-of-diamonds.png"
 }
 ];
+
+
+
+
+
 var cardsInPlay = [];
+
+
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src',cards[cardId].cardImage);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    checkForMatch();
+};
+
 var checkForMatch = function(){
 	if(cardsInPlay.length ===2) {
 	  if (cardsInPlay[0]===cardsInPlay[1]) {
@@ -29,13 +46,20 @@ var checkForMatch = function(){
 	     alert("Sorry, try again");
 	}
 };
-var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-    checkForMatch();	
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+
+//function to create game board
+var createBoard = function(){
+	for (var i = 0; i <cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
 };
 
-flipCard(0);
-flipCard(1);
+//reset game
+
+
+createBoard();
+
